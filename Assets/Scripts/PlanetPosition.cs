5d61b3f3e971a6aa08d99ee724eaca57007b6391
Jan Slifka <jan.slifka@gmail.com>
2017-04-22 14:39:19 +0800

    Add planet position component

---
+++ Assets/Scripts/PlanetPosition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlanetPosition : MonoBehaviour
+{
+	public float distance;
+	public float angle;
+
+	void OnValidate()
+	{
+		angle = Mathf.Clamp(angle, -360, 360);
+		var planetPosition = GameObject.Find("Planet").transform.position;
+		transform.position = planetPosition + new Vector3(0, distance, 0);
+		transform.rotation = Quaternion.identity;
+		transform.RotateAround(planetPosition, Vector3.forward, angle);
+	}
+}

637ebf2e489b09e7f25fdff2f88e45084ca2903d
Jan Slifka <jan.slifka@gmail.com>
2017-04-22 14:17:54 +0800

    Add basic planet movement

---
+++ Assets/Scripts/PlanetMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetMovement : MonoBehaviour
+{
+	public Transform planet;
+	public float movementSpeed;
+
+	protected void MoveRight()
+	{
+		transform.RotateAround(planet.position, Vector3.forward, -movementSpeed * Time.deltaTime);
+	}
+
+	protected void MoveLeft()
+	{
+		transform.RotateAround(planet.position, Vector3.forward, movementSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PlanetMovement
+{
+	void Update()
+	{
+		if (Input.GetAxis("Horizontal") > 0) {
+			MoveRight();
+		} else if (Input.GetAxis("Horizontal") < 0) {
+			MoveLeft();
+		}
+	}
+}

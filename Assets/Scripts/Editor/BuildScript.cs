250f79b94b2fc83a1104906bc65f4f0dae448635
Jan Slifka <jan.slifka@gmail.com>
2017-04-23 20:06:01 +0800

    Add build script

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class BuildScript : MonoBehaviour
+{
+	const string FILE_NAME = "A Small Carrot World";
+
+	static string[] GetScenePaths()
+	{
+		string[] scenes = new string[EditorBuildSettings.scenes.Length];
+
+		for (int i = 0; i < scenes.Length; i++) {
+			scenes[i] = EditorBuildSettings.scenes[i].path;
+		}
+
+		return scenes;
+	}
+
+	[MenuItem("Build/OS X")]
+	static void PerformOSXBuild()
+	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.StandaloneOSXUniversal);
+		BuildPipeline.BuildPlayer(GetScenePaths(), "Build/ascw-osx/" + FILE_NAME, BuildTarget.StandaloneOSXUniversal, BuildOptions.None);
+	}
+
+	[MenuItem("Build/Windows")]
+	static void PerformWindowsBuild()
+	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.StandaloneWindows);
+		BuildPipeline.BuildPlayer(GetScenePaths(), "Build/ascw-win/" + FILE_NAME, BuildTarget.StandaloneWindows, BuildOptions.None);
+	}
+
+	[MenuItem("Build/Build All")]
+	static void BuildAll()
+	{
+		PerformOSXBuild();
+		PerformWindowsBuild();
+	}
+}
